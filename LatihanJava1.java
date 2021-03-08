754e1abb2bad9edfca5018f9ddc019fe98324b42
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-03-08 15:50:13 +0700

    Create LatihanJava1.java

---
+++ LatihanJava1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanjava1;
+
+/**
+ *
+ * @author eldia_000
+ */
+public class LatihanJava1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String Nama,Nim,Asal,Jurusan,Prodi,Pacar;
+        char Golongan;
+        int Usia;
+        boolean Tanda;
+               
+            Nama = "MUHAMMAD ELDIANSYAH PUTRA PRATAMA";
+            Nim = "E41202151";
+            Asal = "Jember";
+            Jurusan = "TEKNILOGI INFORMATIKA";
+            Prodi = "TEKNIK INFORMATIKA";
+            Pacar = "Anya Garaldine";
+            Golongan = 'C';
+            Usia = 18;
+            Tanda = true;
+            
+        System.out.println("Hallo Salam Kenal");
+        System.out.println("NAMA : " + Nama);
+        System.out.println("NIM : " + Nim);
+        System.out.println("ASAL : " + Asal);
+        System.out.println("JURUSAN : " + Jurusan);
+        System.out.println("Pacar : " + Pacar);
+        System.out.println("GOLONGAN : " + Golongan);
+        System.out.println("USIA : " + Usia);
+        System.out.println("TANDA : " + Tanda);
+        
+    }
+    
+}
