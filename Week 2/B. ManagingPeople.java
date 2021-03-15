95a68783b14f40ab081c54fb809ddea79ef51b23
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-03-15 21:20:06 +0700

    Create B. ManagingPeople.java

---
+++ Week 2/B. ManagingPeople.java
@@ -0,0 +1,18 @@
+
+package managingpeople;
+
+public class ManagingPeople {
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Arial", 37);
+        
+        Person p2 = new Person("Josehp", 15);
+        
+        if(p1.getAge() == p2.getAge()){
+            System.out.println(p1.getName() + " is the same age as " + p2.getName());
+        }else{
+            System.out.println(p1.getName() + " is NOT the same age as " + p2.getName());
+        }
+    }
+    
+}
