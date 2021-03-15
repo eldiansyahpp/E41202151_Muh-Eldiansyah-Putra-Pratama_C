7ed1838bd71c7a23b850d938e8d4cf999d1ed2b4
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-03-15 14:45:42 +0700

    Create Latihan2Minggu2.java

---
+++ Week 2/Latihan2Minggu2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2minggu2;
+
+/**
+ *
+ * @author Eldi
+ */
+public class Latihan2Minggu2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "Muh Eldiansyah";
+        char abjad = 'X';
+        int a = 18;
+        double b = 99.9;
+        
+        System.out.println(nama);
+        System.out.println(a);
+    }
+    
+}
