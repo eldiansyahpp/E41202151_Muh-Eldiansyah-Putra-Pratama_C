c150258c7127c3a995fc926435e17f063ea0f85d
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-03-22 18:52:34 +0700

    Create Tugas1Minggu3.java

---
+++ Week 3/Tugas1Minggu3.java
@@ -0,0 +1,14 @@
+package tugas1minggu3;
+
+public class Tugas1Minggu3 {
+
+    public static void main(String[] args) {
+        String[] teks = {"Sugito","Prayoga","Rahardika"};
+        
+        for(int i=0; i<teks.length; i++){
+            System.out.print(teks[i]);
+            System.out.print(" ");
+        }
+    }
+    
+}
