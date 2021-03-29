45928bc071fffef42aab11f6c9fdd21e749b6cb9
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-03-29 16:29:31 +0700

    Create Latihan1Minggu4.java

---
+++ Week 4/Latihan1Minggu4.java
@@ -0,0 +1,13 @@
+package latihan1minggu4;
+
+public class Latihan1Minggu4 {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i <= 10){
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }
+    }
+    
+}
