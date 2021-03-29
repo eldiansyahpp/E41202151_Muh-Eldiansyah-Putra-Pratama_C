371434ba1797600d280803106b474d4e1f4f341c
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-03-29 16:29:44 +0700

    Create Latihan4Minggu4.java

---
+++ Week 4/Latihan4Minggu4.java
@@ -0,0 +1,12 @@
+package latihan4minggu4;
+
+public class Latihan4Minggu4 {
+    public static void main(String[] args) {
+        int angka[] = {3,1,42,24,12};
+        
+        for(int x : angka){
+            System.out.print(x + " ");
+        }
+    }
+    
+}
