97cd8dae47f369f28f32ce68b7154e343766d4a8
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-03-29 16:29:41 +0700

    Create Latihan3Minggu4.java

---
+++ Week 4/Latihan3Minggu4.java
@@ -0,0 +1,14 @@
+package latihan3minggu4;
+
+public class Latihan3Minggu4 {
+    public static void main(String[] args) {
+        
+        for(int i = 0; i <= 5; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
