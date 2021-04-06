cf8a39fac939f8c43167ec3a4ca93d4aea1709b9
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-06 10:58:35 +0700

    Create Latihan2Minggu5.java

---
+++ Week 5/Latihan 2/Latihan2Minggu5.java
@@ -0,0 +1,25 @@
+package latihan2minggu5;
+
+public class Latihan2Minggu5 {
+
+    public static void main(String[] args) {
+        Graph theGraph = new Graph();
+        
+        theGraph.addVertex('A'); //0
+        theGraph.addVertex('B'); //1
+        theGraph.addVertex('C'); //2
+        theGraph.addVertex('D'); //3
+        theGraph.addVertex('E'); //4
+        
+        
+        
+        theGraph.addEdge(0, 1); //AB
+        theGraph.addEdge(1, 2); //BC
+        theGraph.addEdge(2, 3); //AD
+        theGraph.addEdge(3, 4); //DE
+        
+        
+        theGraph.bfs();
+    }
+    
+}
