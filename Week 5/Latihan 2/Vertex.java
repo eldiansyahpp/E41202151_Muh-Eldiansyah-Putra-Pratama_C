a225cbd6d18f92d2f99dfb1591ee74395f5accbd
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-06 10:58:40 +0700

    Create Vertex.java

---
+++ Week 5/Latihan 2/Vertex.java
@@ -0,0 +1,11 @@
+package latihan2minggu5;
+
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertex(char label){
+        this.label = label;
+        wasVisited = false;
+    }
+}
