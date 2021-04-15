9bd21dad1fded08ee1bd53a0fcebf5f76b24af1d
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-15 15:12:49 +0700

    Create Latihan2Minngu6.java

---
+++ Week 6/Latihan2Minngu6.java
@@ -0,0 +1,26 @@
+package latihan2minngu6;
+
+public class Latihan2Minngu6 {
+    public static void main(String[] args) {
+        int[] arr = {2,9,6,7,4,5,3,0,1};
+        int target = 4;
+        sequentialSearch(arr, target);
+    }
+    
+    public static void sequentialSearch(int[] parameterOne, int parameterTwo){
+        int index = -1;
+        
+        for(int i = 0; i < parameterOne.length; i++){
+            if(parameterOne[i] == parameterTwo){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            System.out.println("Your target integer does not exist in the array");
+        } else {
+            System.out.println("Your target integer is in index " + index + " of the array");
+        }
+    }
+    
+}
