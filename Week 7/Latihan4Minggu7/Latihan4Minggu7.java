e8af9c451335ad5f6edc77904f0707545ac3830a
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-20 09:38:16 +0700

    Create Latihan4Minggu7.java

---
+++ Week 7/Latihan4Minggu7/Latihan4Minggu7.java
@@ -0,0 +1,10 @@
+package latihan4minggu7;
+
+public class Latihan4Minggu7 {
+
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.tampilkanKeLayar();
+    }
+    
+}
