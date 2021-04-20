15f9c221ea9cedd195965578a1ecd082267c46dc
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-20 09:38:14 +0700

    Create B.java

---
+++ Week 7/Latihan4Minggu7/B.java
@@ -0,0 +1,9 @@
+package latihan4minggu7;
+
+public class B extends A{
+    @Override
+    public void tampilkanKeLayar(){
+        super.tampilkanKeLayar();
+        System.out.println("Method milik class B dipanggil");
+    }
+}
