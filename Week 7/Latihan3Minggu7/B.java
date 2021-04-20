2675f95529ef103370b09bb0064fa9811135eefe
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-20 09:38:05 +0700

    Create B.java

---
+++ Week 7/Latihan3Minggu7/B.java
@@ -0,0 +1,9 @@
+package latihan3minggu7;
+
+public class B extends A{
+    B(){
+        super("test");
+        
+        System.out.println("Konstruktor class B dieksekusi.................");
+    }
+}
