e3b1e0927fa7db305c3c899aefd4651cd8cc858c
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-20 09:38:22 +0700

    Create B.java

---
+++ Week 7/Latihan5Minggu7/B.java
@@ -0,0 +1,10 @@
+package latihan5minggu7;
+
+public class B extends A{
+
+    @Override
+    public void method2() {
+        System.out.println("Method konkrit yang sudah menjadi konkrit dalam class B");
+    }
+    
+}
