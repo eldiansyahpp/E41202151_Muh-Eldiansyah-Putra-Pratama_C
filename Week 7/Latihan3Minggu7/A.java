e656937465627900b0efa592797ac0b2e207f694
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-20 09:38:03 +0700

    Create A.java

---
+++ Week 7/Latihan3Minggu7/A.java
@@ -0,0 +1,9 @@
+package latihan3minggu7;
+
+public class A {
+    A(String param1){
+        super();
+        
+        System.out.println("Konstruktor class A dengan parameter " + param1 + " dieksekusi...........");
+    }
+}
