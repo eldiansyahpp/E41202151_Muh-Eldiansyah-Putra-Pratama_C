fa5325d21b02a87ecd8c1c67c8948b3fd9d7494c
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-20 09:38:24 +0700

    Create Latihan5Minggu7.java

---
+++ Week 7/Latihan5Minggu7/Latihan5Minggu7.java
@@ -0,0 +1,11 @@
+package latihan5minggu7;
+
+public class Latihan5Minggu7 {
+
+    public static void main(String[] args) {
+        B ob = new B();
+        ob.method1();
+        ob.method2();
+    }
+    
+}
