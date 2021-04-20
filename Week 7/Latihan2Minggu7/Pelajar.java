acacfa770bcfb64bd0576669e9c450e3c16adb79
eldiansyahpp <75104104+eldiansyahpp@users.noreply.github.com>
2021-04-20 09:38:00 +0700

    Create Pelajar.java

---
+++ Week 7/Latihan2Minggu7/Pelajar.java
@@ -0,0 +1,18 @@
+package latihan2minggu7;
+
+public class Pelajar extends Orang{
+    private String nim;
+    private String asalSekolah;
+    private double nilai;
+    
+    public Pelajar(String nama, double tinggi, double berat, String nim,String sekolah, double nilai){
+        super(nama, tinggi, berat);
+        this.nim = nim;
+        asalSekolah = sekolah;
+        this.nilai = nilai;
+    }
+    
+    public String toString(){
+        return(super.toString() + "\nNIM : " + nim + "\nSekolah : " + asalSekolah + "\nNilai : " + nilai);
+    }
+}
